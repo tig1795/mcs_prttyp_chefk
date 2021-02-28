d583ac983c11b43ec7d83be5b65f1ace4334fd3d
jankoe94 <58293469+jankoe94@users.noreply.github.com>
2021-02-28 10:05:22 +0100

    login hatte falschen link im chefkochbild
    jetzt passts wieder

--- files/PHP/login2.php
+++ files/PHP/login2.php
@@ -8,7 +8,7 @@
     <header>
         <div class="row">
             <div class="logo-row">
-              <a href="2.php">
+              <a href="index2.php">
                <img src="../../images/chefkoch-logo_1-1-30.png" alt="logo" class="logo">
               </a>
             </div>
